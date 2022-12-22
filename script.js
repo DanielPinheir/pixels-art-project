a51f99229658a2feae43624cb2a2a0dbb4b26ff1
Daniel Pinheiro Machado <dyelzim@gmail.com>
2022-12-22 19:23:41 -0300

    challenge 2 - add color palette

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
+// escopo global
+const getBody = document.querySelector('body');
+
 // challenge1
+const addHeader = () => {
+  const elementHeader = document.createElement('header');
+  getBody.appendChild(elementHeader);
+};
+addHeader();
+
 const addTitle = () => {
-  const getBody = document.getElementsByTagName('body')[0];
+  const getHeader = document.querySelector('header');
   const elementH1 = document.createElement('h1');
   elementH1.id = 'title';
   elementH1.innerHTML = 'Paleta de Cores';
-  getBody.appendChild(elementH1);
+  elementH1.style.textAlign = 'center';
+  getHeader.appendChild(elementH1);
 };
 addTitle();
+
+// challenge2
+const addDiv = () => {
+  const elementDiv = document.createElement('div');
+  elementDiv.id = 'color-palette';
+  getBody.appendChild(elementDiv);
+};
+addDiv();
+
+const addColorPalette = () => {
+  const colorPalette = document.getElementById('color-palette');
+
+  for (let index = 0; index < 4; index += 1) {
+    const elementDiv = document.createElement('div');
+    elementDiv.className = 'color';
+    elementDiv.style.border = 'solid 1px black';
+    elementDiv.style.width = '50px';
+    elementDiv.style.height = '50px';
+    elementDiv.style.borderRadius = '50%';
+    const colorRed = Math.floor(Math.random() * 255);
+    const colorGreen = Math.floor(Math.random() * 255);
+    const colorBlue = Math.floor(Math.random() * 255);
+    elementDiv.style.backgroundColor = `rgb(${colorRed}, ${colorGreen}, ${colorBlue})`;
+
+    colorPalette.appendChild(elementDiv);
+  }
+};
+addColorPalette();
